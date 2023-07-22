f3bdacd7f57eae616fe34599b9edc6ea7d8723bf
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-07-22 18:50:41 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import { Component } from 'react';
+
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loading from './Loader/Loader';
+import Modal from './Modal/Modal';
+
 export const App = () => {
   return (
     <div
@@ -7,7 +15,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
